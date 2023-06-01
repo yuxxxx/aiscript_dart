b3cf1ce9269322ebc1361247b85c0b6761b6186a
yuxxxx <yuunnxx@gmail.com>
2023-06-02 03:45:01 +0900

    add: project setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # aiscript_dart
+
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
---
+++ bin/aiscript_dart.dart
@@ -0,0 +1,5 @@
+import 'package:aiscript_dart/aiscript_dart.dart' as aiscript_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aiscript_dart.calculate()}!');
+}
