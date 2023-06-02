a019eb60d87918ca6980edee35970521a7f0eef1
yuxxxx <yuunnxx@gmail.com>
2023-06-03 00:25:24 +0900

    自然数の四則演算と簡単な文字列・数の比較ができる

--- lib/aiscript_dart.dart
+++ lib/aiscript_dart.dart
@@ -1,3 +1,66 @@
+import 'package:petitparser/petitparser.dart';
+
 int calculate() {
   return 6 * 7;
+}
+
+dynamic parseNumber(text) {
+  final integer = digit().plus().flatten().trim().map(int.parse);
+  final real = (digit().plus() & char('.') & digit().plus())
+      .flatten()
+      .trim()
+      .map(double.parse);
+  final number = (real | integer);
+
+  final term = undefined();
+  final prod = undefined();
+  final prim = undefined();
+
+  final minus =
+      (prod & char('-').trim() & term).map((values) => values[0] - values[2]);
+  final add =
+      (prod & char('+').trim() & term).map((values) => values[0] + values[2]);
+  term.set(add | minus | prod);
+
+  final mul =
+      (prim & char('*').trim() & prod).map((values) => values[0] * values[2]);
+  final div =
+      (prim & char('/').trim() & prod).map((values) => values[0] / values[2]);
+  prod.set(mul | div | prim);
+
+  final parens =
+      (char('(').trim() & term & char(')').trim()).map((values) => values[1]);
+  prim.set(parens | number);
+  final parser = term.end();
+  return parser.parse(text).value;
+}
+
+dynamic parseCompare(text) {
+  final integer = digit().plus().flatten().trim().map(int.parse);
+  final real = (digit().plus() & char('.') & digit().plus())
+      .flatten()
+      .trim()
+      .map(double.parse);
+  final number = (real | integer);
+  final letters = (char('"') & word().plus().flatten() & char('"'))
+      .map((values) => values[1] as String);
+  final term = undefined();
+
+  final lt = (number & char('>').trim() & number)
+      .map((values) => values[0] > values[2]);
+
+  final gt = (number & char('<').trim() & number)
+      .map((values) => values[0] < values[2]);
+
+  final equatable = (number | letters);
+  final eq = (equatable & string('==').trim() & equatable)
+      .map((values) => values[0] == values[2]);
+
+  final ne = (equatable & string('!=').trim() & equatable)
+      .map((values) => values[0] != values[2]);
+
+  term.set(eq | ne | lt | gt | equatable);
+  final parser = term.end();
+
+  return parser.parse(text).value;
 }
--- test/aiscript_dart_test.dart
+++ test/aiscript_dart_test.dart
@@ -5,4 +5,64 @@ void main() {
   test('calculate', () {
     expect(calculate(), 42);
   });
+
+  test('parse add', () {
+    expect(parseNumber('1 + 2'), 3);
+  });
+
+  test('parse minus', () {
+    expect(parseNumber('1 - 2'), -1);
+  });
+
+  test('parse multiple', () {
+    expect(parseNumber('1 + 2 * 3'), 7);
+  });
+
+  test('parse div', () {
+    expect(parseNumber('1 / 2'), 0.5);
+  });
+
+  test('parse div0', () {
+    expect(parseNumber('1 / 0'), double.infinity);
+  });
+
+  test('parse div00', () {
+    expect(parseNumber('0 / 0'), isNaN);
+  });
+
+  test('parse compare lt', () {
+    expect(parseCompare('3 > 2'), true);
+  });
+
+  test('parse compare gt', () {
+    expect(parseCompare('2 < 3'), true);
+  });
+
+  test('parse compare eq number', () {
+    expect(parseCompare('2 == 3'), false);
+  });
+
+  test('parse eq string', () {
+    expect(parseCompare('"2e" == "2e"'), true);
+  });
+
+  test('parse compare ne number', () {
+    expect(parseCompare('2 != 3'), true);
+  });
+
+  test('parse ne string', () {
+    expect(parseCompare('"2e" != "2e"'), false);
+  });
+
+  test('parse string', () {
+    expect(parseCompare('"text"'), 'text');
+  });
+
+  test('parse integer', () {
+    expect(parseCompare('42'), 42);
+  });
+
+  test('parse real', () {
+    expect(parseCompare('4.2'), 4.2);
+  });
 }
